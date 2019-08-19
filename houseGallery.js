8f1a7f76720cc07a27ca89e13f9435a23f1236e0
Manuel Garcia <mgarc881@fiu.edu>
2019-08-19 12:43:45 -0400

    added carousel and fixed two pictures

--- houseGallery.js
+++ houseGallery.js
@@ -3,7 +3,8 @@ let images = ["./images/entrance2.JPG","./images/Entrance.JPG",
 "./images/Livingroom.JPG","./images/kitchen1.JPG","./images/kitchen2.JPG","./images/kitchen3.JPG",
 "./images/kitchen4.JPG","./images/rooftop.JPG","./images/terrace.JPG","./images/yard1.JPG","./images/yard2.JPG",
 "./images/ec2b489a-7d49-4d49-bfe7-469f3328b74e.JPG","./images/bathroom1.JPG","./images/Bedroom1.JPG","./images/bedroom2.JPG",
-"./images/DinningRoom.JPG","./images/bedroom5.JPG"];
+"./images/DinningRoom.JPG","./images/bedroom5.JPG","./images/mainLivingRoom.JPG"
+,"./images/anotherLivingroom.JPG"];
 console.log(images.length);
 
 $('.view-btn').click((e)=>{
